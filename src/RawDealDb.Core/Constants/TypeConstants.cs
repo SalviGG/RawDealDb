5f426c9621d8ab68038066014f911d446e489aa6
SalviGG <kei.miskatonic@gmai.com>
2023-02-21 16:08:52 -0300

    Added type parsing and color assignment for some card types.

---
+++ src/RawDealDb.Core/Constants/TypeConstants.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RawDealDb.Core.Constants
+{
+    public static class TypeConstants
+    {
+        public static List<string> ManeuverTypes = new List<string>
+        {
+            "Strike",
+            "Grapple",
+            "Submission",
+            "Trademark Finisher"
+        };
+
+        public const string ManeuverColor = "#bfa80a";
+        public const string ActionColor = "#7d9cd2";
+        public const string ReversalColor = "#ba2018";
+        public const string Prematch = "#4d325d";
+
+    }
+}
--- src/RawDealDb.Core/DTO/CardDto.cs
+++ src/RawDealDb.Core/DTO/CardDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RawDealDb.Core.Constants;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,6 +22,44 @@ namespace RawDealDb.Core.DTO
         public string SuperStarLogo { get; set; }
         public Guid SetId { get; set; }
         public string SetName { get; set; }
+
+        public int GetTypesQty
+            ()
+        {
+            var splitTypes = CardType.Trim().Split("/");
+            return splitTypes.Count();
+        }
+
+        public string ParseType()
+        {
+            var typesQty = GetTypesQty();
+
+            string cardTypeParsed = "";
+            if ((CardType.Equals("strike", StringComparison.OrdinalIgnoreCase) ||
+            CardType.Equals("grapple", StringComparison.OrdinalIgnoreCase) ||
+            CardType.Equals("submission", StringComparison.OrdinalIgnoreCase)) && typesQty == 1)
+            {
+                cardTypeParsed = "Maneuver";
+            }
+            else if (CardType.Equals("action", StringComparison.OrdinalIgnoreCase) && typesQty == 1 )
+            {
+                cardTypeParsed = "Action";
+            }
+            else if (CardType.Contains("Reversal") && typesQty == 1)
+            {
+                cardTypeParsed = "Reversal";
+            }
+            else if (typesQty > 1 && TypeConstants.ManeuverTypes.Any(x => CardType.Contains(x)) && CardType.Contains("Reversal"))
+            {
+                cardTypeParsed = "Maneuver Reversal";
+            }
+            else if (CardType.Contains("Pre-match"))
+            {
+                cardTypeParsed = "Pre-match";
+            }
+
+            return cardTypeParsed;
+        }
 
     }
 }
